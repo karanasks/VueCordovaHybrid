77986e26ecd0b8946984d62f2ba432d518cfe650
Karan Fatnani <karanworks.987@gmail.com>
2019-02-05 13:19:49 +0530

    Reoved custom code for file selection

--- leaflet-demo/src/main.js
+++ leaflet-demo/src/main.js
@@ -10,25 +10,7 @@ Vue.config.productionTip = false
 document.addEventListener('deviceready', onDeviceReady, false)
 
 function onDeviceReady () {
-  function readFromFile (fileName, cb) {
-    var pathToFile = cordova.file.dataDirectory + fileName
-    window.resolveLocalFileSystemURL(pathToFile, function (fileEntry) {
-      fileEntry.file(function (file) {
-        var reader = new FileReader()
 
-        reader.onloadend = function (e) {
-          cb(JSON.parse(this.result))
-        }
-
-        reader.readAsText(file)
-      }, errorHandler.bind(null, fileName))
-    }, errorHandler.bind(null, fileName))
-  }
-
-  var fileData
-  readFromFile('data.json', function (data) {
-    fileData = data
-  })
 }
 
 /* eslint-disable no-new */
